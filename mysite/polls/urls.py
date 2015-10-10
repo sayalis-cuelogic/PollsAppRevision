e3ff1ad1c10c65bf5a8f1ccedc2cd1ab7bb371f8
sayalis-cuelogic <sayali.shete@cuelogic.co.in>
2015-10-10 14:50:26 +0530

    Created tests for questions

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
-    # ex: /polls/5/
-    url(r'^(?P<question_id>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
-    # ex: /polls/5/results/
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
-    # ex: /polls/5/vote/
-    url(r'^(?P<question_id>[0-9]+)/vote/$', views.VoteView.as_view(), name='vote'),
+    url(r'^(?P<question_id>[0-9]+)/$',
+        views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<question_id>[0-9]+)/results/$',
+        views.ResultsView.as_view(), name='results'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$',
+        views.VoteView.as_view(), name='vote'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect, Http404
 from django.core.urlresolvers import reverse
 from django.views import generic
+from django.utils import timezone
 
 from polls.models import Question, Choice
 # Create your views here.
@@ -12,8 +13,8 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by(
+            '-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
